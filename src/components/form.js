7ad1b6ebfe3cba4fa3f4b5301f66283dc4547428
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-01-04 16:30:54 +0530

    First Commit

---
+++ src/components/form.js
@@ -0,0 +1,9 @@
+function Form() {
+    return (
+        <form className="add-todo">
+            <input className="add-todo--input" type="text"></input>
+            <button type="submit" className="add-todo--btn">add</button>
+        </form>
+    );
+}
+export default Form;
